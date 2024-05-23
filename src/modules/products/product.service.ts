b54ac15237cffe7079f9d2703730e5801972936a
harunjk <harunrashidcpa@gmail.com>
2024-05-23 15:02:46 +0000

    router

--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -31,7 +31,13 @@ const DeleteSingleProduct = async (_id: string) => {
   return result;
 };
 
-const saerchProduct = async (searData: string) => {};
+const saerchProduct = async (searData: string) => {
+  const regex = new RegExp(searData, "i");
+  const result = await Products.find({
+    $or: [{ name: { $regex: regex } }],
+  });
+  return result;
+};
 
 export const ProductService = {
   createProductSr,
